fd184dfc7309e99c1d15c278756bab574866b9bc
Shubham Chauhan <shubhamchauhan@Shubhams-MacBook-Air.local>
2025-02-13 07:03:06 -0800

    SEND OTP SCREEN Complete UI

--- app/src/main/java/com/example/cricket/common/CommonUI.kt
+++ app/src/main/java/com/example/cricket/common/CommonUI.kt
@@ -1,9 +1,18 @@
 package com.example.cricket.common
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -11,16 +20,20 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
 import com.example.cricket.R
-
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.ui.text.TextStyle
 
 
 // common Image function for loading SVG File and GIF file
@@ -51,24 +64,120 @@ fun CommonImage(
 
 // common Text View for storing dynamic value
 @Composable
-fun CommonTextView(text: String,
-                   modifier: Modifier = Modifier,
-                   fontSize: TextUnit = 16.sp, // Default font size
-                   fontWeight: FontWeight = FontWeight.Normal, // Default font weight
-                   color: Color = Color.Black, // Default text color
-                   textAlign: TextAlign? = null, // Optional text alignment
-                   maxLines: Int = Int.MAX_VALUE, // Default: No line limit
-                   overflow: TextOverflow = TextOverflow.Ellipsis // Handles overflow text
- ){
+fun CommonTextView( text: String,
+                    modifier: Modifier = Modifier,
+                    fontSize: TextUnit = 16.sp, // Default font size
+                    fontWeight: FontWeight = FontWeight.Normal, // Default font weight
+                    color: Color = Color.Black, // Default text color
+                    textAlign: TextAlign? = null, // Optional text alignment
+                    maxLines: Int = Int.MAX_VALUE, // Default: No line limit
+                    overflow: TextOverflow = TextOverflow.Ellipsis, // Handles overflow text
+                    paddingStart: Dp = 0.dp, // Default padding for start
+                    paddingEnd: Dp = 0.dp,   // Default padding for end
+                    paddingTop: Dp = 0.dp,   // Default padding for top
+                    paddingBottom: Dp = 0.dp // Default padding for bottom
+){
     Text(
         text = text,
-        modifier = modifier,
+        modifier = modifier.padding(start = paddingStart, end = paddingEnd, top = paddingTop, bottom = paddingBottom),
+
         fontSize = fontSize,
         fontWeight = fontWeight,
         color = color,
         textAlign = textAlign,
         maxLines = maxLines,
-        overflow = overflow
+        overflow = overflow,
+
     )
 
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CommonTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    placeholder: String = "Enter text...",
+    textColor: Color = Color.Black,
+    placeholderColor: Color = Color.Gray,
+    borderColor: Color = colorResource(id = R.color.pink), // Default border color
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default, // Dynamic keyboard options
+    visualTransformation: VisualTransformation = VisualTransformation.None, // Allows password masking
+    paddingStart: Dp = 0.dp,
+    paddingEnd: Dp = 0.dp,
+    paddingTop: Dp = 0.dp,
+    paddingBottom: Dp = 0.dp
+) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        modifier = modifier
+            .padding(
+                start = paddingStart,
+                end = paddingEnd,
+                top = paddingTop,
+                bottom = paddingBottom
+            )
+            .fillMaxWidth(),
+        textStyle = TextStyle(color = textColor, fontSize = 16.sp),
+        placeholder = { Text(text = placeholder, color = placeholderColor) },
+        keyboardOptions = keyboardOptions,
+        visualTransformation = visualTransformation, // Handles transformations (e.g., password)
+        singleLine = true,
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = borderColor,  // Color when focused
+            unfocusedBorderColor = borderColor // Color when not focused
+        )
+    )
+}
+
+
+@Composable
+fun AppButton(
+    title: String,
+    paddingStart: Dp = 0.dp,
+    paddingEnd: Dp = 0.dp,
+    paddingTop: Dp = 0.dp,
+    paddingBottom: Dp = 0.dp,
+    textColor: Color = Color.White,
+    background: Color = Color.Blue, // Default background color
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = {}
+) {
+    Button(
+        onClick = onClick,
+        shape = RoundedCornerShape(10.dp),
+        elevation = ButtonDefaults.buttonElevation(0.dp), // Updated for Material3
+        colors = ButtonDefaults.buttonColors(
+            containerColor = background // Updated from backgroundColor
+        ),
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(
+            start = paddingStart,
+            end = paddingEnd,
+            top = paddingTop,
+            bottom = paddingBottom
+        )
+    ) {
+        Text(
+            text = title,
+            style = TextStyle(
+                fontSize = 14.sp,
+                color = textColor,
+                fontWeight = FontWeight.W600
+            )
+        )
+    }
+}
+
+@Composable
+fun SpacerHeight(height: Dp = 5.dp) {
+    Spacer(modifier = Modifier.height(height))
+}
+
+@Composable
+fun SpacerWidth(width: Dp = 5.dp) {
+    Spacer(modifier = Modifier.width(width))
 }
--- app/src/main/java/com/example/cricket/screen/SendOTP.kt
+++ app/src/main/java/com/example/cricket/screen/SendOTP.kt
@@ -3,17 +3,33 @@ package com.example.cricket.screen
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.cricket.R
+import com.example.cricket.common.AppButton
 import com.example.cricket.common.CommonImage
+import com.example.cricket.common.CommonTextField
+import com.example.cricket.common.CommonTextView
 
 @Preview(showSystemUi = true, showBackground = true)
 @Composable
 fun SendOTP(){
+
+    val mobileNumber = rememberSaveable { mutableStateOf("") }
 
     Box(modifier = Modifier
         .fillMaxSize()
@@ -21,7 +37,64 @@ fun SendOTP(){
         Column{
 
             CommonImage(imageUrl = "https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif")
+            CommonTextView(
+                text = "Verify your Mobile Number",
+                paddingStart = 16.dp,
+                paddingEnd = 0.dp,
+                paddingTop = 6.dp,
+                paddingBottom = 0.dp,
+                fontSize = 28.sp,
+                fontWeight = FontWeight.Bold,
+                color = colorResource(id = R.color.white),
+                textAlign = TextAlign.Center,
+                maxLines = 2,
+            )
 
+            CommonTextView(
+                text = "We have send you OTP on this mobile number",
+                paddingStart = 16.dp,
+                paddingEnd = 0.dp,
+                paddingTop = 20.dp,
+                paddingBottom = 0.dp,
+                fontSize = 12.sp,
+                fontWeight = FontWeight.Normal,
+                color = colorResource(id = R.color.white),
+                textAlign = TextAlign.Center,
+                maxLines = 2,
+            )
+
+            Spacer(modifier = Modifier.padding(top = 10.dp))
+
+
+            CommonTextField(
+                value = mobileNumber.value,
+                onValueChange = { newText ->
+                    if (newText.all { it.isDigit() }) { // Allow only numeric input
+                        mobileNumber.value = newText
+                    }
+                },
+                placeholder = "Enter Mobile Number",
+                placeholderColor = colorResource(id = R.color.white),
+                textColor = colorResource(id = R.color.white),
+                borderColor = colorResource(id = R.color.white),
+                paddingStart = 16.dp,
+                paddingEnd = 16.dp,
+                paddingTop = 26.dp,
+                paddingBottom = 0.dp,
+                keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number)
+            )
+
+            Spacer(modifier = Modifier.padding(top = 10.dp))
+            AppButton(
+                title = "Send OTP",
+                background = Color.White,
+                textColor = Color.Black,
+                paddingStart = 16.dp,
+                paddingEnd = 16.dp,
+                paddingTop = 16.dp,
+                paddingBottom = 0.dp,
+                onClick = { /* Submit Action */ }
+            )
 
         }
     }
